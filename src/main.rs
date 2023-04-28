88a6031f7268ba8eafb13a98ab02877873b92824
JuliaJHL <93232947+JuliaJHL@users.noreply.github.com>
2023-04-28 23:57:22 +0000

    modify index.html

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,43 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer};
-use rust_bert::pipelines::text_generation::TextGenerationModel;
-use rust_bert::pipelines::common::ModelType;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_files::Files;
+use serde::{Deserialize, Serialize};
+// use rust_bert::pipelines::text_generation::TextGenerationModel;
+// use rust_bert::pipelines::common::ModelType;
 
 
 #[get("/")]
-async fn index() -> HttpResponse {
+async fn index() -> impl Responder {
+    println!("hello");  // This is printed in the console for test
     HttpResponse::Ok()
         .content_type("text/html")
         .body(include_str!("../templates/index.html"))
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct Response {
+    output: String,
+}
+
 //  Natural Language Generation function
-#[derive(serde::Deserialize)]
-struct FormData {
+#[derive(Debug, Serialize, Deserialize)]
+struct GenerateRequest {
     prompt: String,
 }
 
-#[get("/text_generator")]
-async fn show_generator() -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(include_str!("../templates/generate.html"))
-}
 
 #[post("/generator")]
-async fn generator(form: web::Form<FormData>) -> HttpResponse {
-    let prompt = form.prompt.to_owned();
-    let model = TextGenerationModel::new(Default::default());
-    let output = model.expect("REASON").generate(&[prompt], None);
-    let output = output[0].to_owned();
+async fn generator(info: web::Json<GenerateRequest>) ->  impl Responder {
+    println!("gogogo");
+    let prompt = info.prompt.to_owned();
+    println!("prompt: {}", prompt);
+    // let model = TextGenerationModel::new(Default::default());
+    // let output = model.expect("REASON").generate(&[prompt], None);
+    // let output = output[0].to_owned();
+    let output = format!("{}{}", "prefix", prompt);
+    let reponse = Response { output };
 
-    HttpResponse::Ok()
-        .content_type("text/html")
-        .body(format!("{}", output))
+    HttpResponse::Ok().json(reponse)
 }
-
-
-
-
-
 
 
 
@@ -48,8 +47,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(index)
-            .service(show_generator)
             .service(generator)
+            .service(Files::new("/static", "./templates"))
     })
     .bind("127.0.0.1:8080")?
     .run()
